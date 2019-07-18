2e557e1f96e73d5b2f3b9f1c05770cfc8fd31a9a
Walker Cole <besidethebrave@gmail.com>
2019-07-17 21:54:13 -0400

    Initial commit - Pre-MongoDB Integration

---
+++ server/config/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+    seedData: true,
+    dbConnection: "mongodb://127.0.0.1:27017/task-management-system",
+    numberOfBoards: 15,
+    numberOfListsPerBoard: 8,
+    numberOfCardsPerList: 4
+}
